e8337cda50e846b7b2dc25b8f40df94924f07385
Dragoi Ovidiu <dragoiovidiu2011@yahoo.com>
2017-05-16 17:05:47 +0300

    Added a documentation link

--- ApiGateway/src/Application/ApiBundle/Controller/DefaultController.php
+++ ApiGateway/src/Application/ApiBundle/Controller/DefaultController.php
@@ -3,11 +3,16 @@
 namespace Application\ApiBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Application\ApiBundle\Util\ApiResponse;
+use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller
 {
-    public function indexAction()
+    public function getTokenAction(Request $request)
     {
-        return $this->render('ApplicationApiBundle:Default:index.html.twig');
+    	$loginData["message"] = "ok";
+    	$loginData["status"] = 200;
+
+        return ApiResponse::setResponse($loginData["message"], $loginData["status"]);
     }
 }
